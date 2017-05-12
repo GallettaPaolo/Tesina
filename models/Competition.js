2e6af70e4fba53acd979f881884cc755f0c14196
Pool <galletta.paolo98@gmail.com>
2017-05-12 12:02:25 +0200

    Prelevamento dati corretto da mongodb

---
+++ models/Competition.js
@@ -0,0 +1,9 @@
+function Competition(code,date,scale,description,type,location){
+    this.code = code;
+    this.date = date;
+    this.scale = scale;
+    this.description = description;
+    this.type = type;
+    this.location = location;   
+}
+module.exports =  Competition;
--- models/MongoHelper.js
+++ models/MongoHelper.js
@@ -1,9 +1,8 @@
 /**
  * Classe Per l'iterazione con il Database (Singleton)
  */
-
 var singleton = null;
-
+var Competition = require("./Competition.js");
 /**
  * Costruttore dell'helper
  */
@@ -13,7 +12,7 @@ function MongoHelper(){
      * Campi privati
      */
     var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://localhost:27017";
+    var url = "mongodb://localhost:27017/athletics";
     var assert = require("assert");
     var database;
     /**
@@ -27,16 +26,36 @@ function MongoHelper(){
         return url;
     }
 
+    /**
+     * Funzione che ritorna tutte le competizioni 
+     */
     this.getCompetitions = ()=>{
         MongoClient.connect(url, function(err, db) {
-        assert.equal(null, err);
-        var competitios = db.collection('competitions');
-        console.log(competitions);
-        db.close();
-});
+            assert.equal(null, err);
+            console.log("Connessione effettuata");
+            findCompetitions(db,()=>{
+                db.close();
+            })
+        });
     }
+
+    var findCompetitions = (db,callback)=>{
+        var competitionsColl = db.collection('competitions');
+        var competitions = [];
+        competitionsColl.find({}).toArray((err,coll)=>{
+            coll.forEach(function(element) {
+                competitions.push(new Competition(element.code,element.date,element.scale,element.description,element.type,element.location));
+            }, this);
+            console.log(competitions);
+            callback();
+        });
+    }
+
 };
 
+/**
+ * Funzione che viene esportata (Viene utilizzare per il pattern singleton)
+ */
 module.exports = function getHelper(){
     return (singleton == null)? new MongoHelper():singleton;
 }
